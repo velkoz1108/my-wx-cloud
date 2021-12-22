f163422f00b1c44097b1f75fb4041bcc1a4ac8be
Wang, Tao <twang@telenav.cn>
2021-12-22 17:12:36 +0800

    add a new api

---
+++ src/main/java/com/tencent/wxcloudrun/controller/TestController.java
@@ -0,0 +1,45 @@
+package com.tencent.wxcloudrun.controller;
+
+import com.tencent.wxcloudrun.config.ApiResponse;
+import com.tencent.wxcloudrun.dto.CounterRequest;
+import com.tencent.wxcloudrun.model.Counter;
+import com.tencent.wxcloudrun.service.CounterService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+/**
+ * counter控制器
+ */
+@RestController
+
+public class TestController {
+
+    final CounterService counterService;
+    final Logger logger;
+
+    public TestController(@Autowired CounterService counterService) {
+        this.counterService = counterService;
+        this.logger = LoggerFactory.getLogger(TestController.class);
+    }
+
+
+    /**
+     * 获取当前计数
+     *
+     * @return API response json
+     */
+    @GetMapping(value = "/api/test")
+    ApiResponse get() {
+        logger.info("/api/test get request");
+        return ApiResponse.ok("EdenWang");
+    }
+
+
+}
